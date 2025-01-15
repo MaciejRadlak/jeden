10a6a6aa3a52235e05c08fbb85366f3400bc7766
MaciejRadlak <maciollo07@gmail.com>
2025-01-15 14:18:26 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
+    const handleNameChange = ()=> {
+      const names=['Bob','kevin','Dave'];
+      const int =Math.floor (Math.random()*3);
+      return names[int]
+    }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>lubie pociagi</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          
-        >
-          Learn Reactgds g
-        </a>
-      </header>
+      <Header/>
+      <Content/>
+      <Footer/>
     </div>
+    
+        
+      
   );
 }
 
