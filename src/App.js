c044f654217d21e8840e2e1ee4939e3e760bf399
MaciejRadlak <maciollo07@gmail.com>
2025-04-02 13:23:34 +0200

    baza danych

--- src/App.js
+++ src/App.js
@@ -4,29 +4,52 @@ import SearchItem from './SearchItem';
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+  const API_URL = 'http://localhost:3500/items';
 
+  const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState('');
-  const[search,setSearch]=useState('')
+  const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState('');
 
-  const setAndSaveItems=(newItems)=>{
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if(!response.ok) {
+          throw Error('Did not received expected data');
+        }
+        const listItems = await response.json();
+        setItems(listItems);
+        console.log(listItems);
+        setFetchError(null);
+      } catch (err) {
+        console.log(err.message);
+        setFetchError(err.message);
+        
+      }
+    };
+    fetchItems();
+  }, []);
+
+  const setAndSaveItems = (newItems) => {
     setItems(newItems);
     localStorage.setItem('shoppinglist', JSON.stringify(newItems));
-  }
+  };
 
-  const addItem=(item)=>{
-    const id= items.length? items[items.length-1].id+1: 1;
-    const myNewItem={id,checked:false,item};
-    const listItems=[...items,myNewItem];
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
     setAndSaveItems(listItems);
-  }
+  };
 
   const handleCheck = (id) => {
-    console.log(`key: ${id}`);
-    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
     setAndSaveItems(listItems);
   };
 
@@ -37,7 +60,7 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!newItem) return;
+    if (!newItem) return;
     addItem(newItem);
     setNewItem('');
   };
@@ -45,21 +68,18 @@ function App() {
   return (
     <div className="App">
       <Header title="Grocery" />
-      <AddItem
-        newItem={newItem}
-        setNewItem={setNewItem}
-        handleSubmit={handleSubmit}
-      />
-      <SearchItem
-        search={search}
-        setSearch={setSearch}
-      />
-      <Content
-        items={items.filter(item =>((item.item).toLowerCase()).includes
-        (search.toLocaleLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      <AddItem newItem={newItem} setNewItem={setNewItem} handleSubmit={handleSubmit} />
+      <SearchItem search={search} setSearch={setSearch} />
+      <main>
+        {fetchError && <p style={{color:"red"}}>{`Error: ${fetchError}`}</p>}
+        <Content
+          items={items.filter((item) =>
+            item.item.toLowerCase().includes(search.toLowerCase())
+          )}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      </main>
       <Footer length={items.length} />
     </div>
   );
